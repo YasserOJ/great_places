944909ffd67104ba9b7395cea06e2047b03fd0c4
fedigmd1 <gammoudifedi@gmail.com>
2020-01-14 14:17:54 +0100

    Managing data and images via the provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:great_places/screens/places_list_screen.dart';
 import 'package:provider/provider.dart';
+
+import './providers/great_places.dart';
+import './screens/places_list_screen.dart';
 import './screens/add_place_screen.dart';
 
 void main() => runApp(MyApp());
@@ -8,20 +10,19 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (BuildContext context) {},
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
       child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Great Places',
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-          accentColor: Colors.amber,
-        ),
-        home: PlacesListScreen(),
-        routes: {
-          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
-        },
-      ),
+          debugShowCheckedModeBanner: false,
+          title: 'Great Places',
+          theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            accentColor: Colors.amber,
+          ),
+          home: PlacesListScreen(),
+          routes: {
+            AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+          }),
     );
   }
 }
