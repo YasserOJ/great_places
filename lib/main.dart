23fc248e8dfecb00d0b15d6aa293dfb936ad28ac
fedigmd1 <gammoudifedi@gmail.com>
2020-01-14 10:30:47 +0100

    Adding the add place scrren and an image input

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/screens/places_list_screen.dart';
 import 'package:provider/provider.dart';
+import './screens/add_place_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,14 +9,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      //create: (BuildContext context) {  },
+      create: (BuildContext context) {},
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Great Places',
         theme: ThemeData(
           primarySwatch: Colors.indigo,
           accentColor: Colors.amber,
         ),
-        //home: ...,
+        home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+        },
       ),
     );
   }
